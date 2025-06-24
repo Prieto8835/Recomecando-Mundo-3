782927ec4e89b6b3c21de0bf2d12f80e63fcb4ea
Alejandro Ribeiro Prieto <147511896+Prieto8835@users.noreply.github.com>
2025-06-23 22:24:33 -0300

    Add files via upload

---
+++ exercicios/exercicio97.py
@@ -0,0 +1,10 @@
+def linha(txt):
+    tamanho = len(txt) + 14
+    print('~' * tamanho)
+    print(f'       {txt}')
+    print('~' * tamanho)
+
+
+linha('Alejandro Ribeiro Prieto')
+linha('Curso de Python no YouTube')
+linha('CeV')
